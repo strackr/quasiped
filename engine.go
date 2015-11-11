203ac2ef1294335c3e9d63d05c077cc1efab9b11
Robert Strack <robert.strack@gmail.com>
2015-11-10 22:25:19 -0500

    added servo structure

--- engine.go
+++ engine.go
@@ -25,21 +25,25 @@ type location struct {
 	orientation orientation
 }
 
-type Engine struct {
-	servos map[location]*gpio.ServoDriver
+type engine struct {
+	servos map[location]*servo
 }
 
-func NewEngine(w gpio.ServoWriter) *Engine {
-	return &Engine{
-		servos: map[location]*gpio.ServoDriver{
-			location{nw, horizontal}: gpio.NewServoDriver(w, "nw_h", "1"),
-			location{ne, horizontal}: gpio.NewServoDriver(w, "ne_h", "2"),
-			location{sw, horizontal}: gpio.NewServoDriver(w, "sw_h", "3"),
-			location{se, horizontal}: gpio.NewServoDriver(w, "se_h", "4"),
-			location{nw, vertical}:   gpio.NewServoDriver(w, "nw_v", "5"),
-			location{ne, vertical}:   gpio.NewServoDriver(w, "ne_v", "6"),
-			location{sw, vertical}:   gpio.NewServoDriver(w, "sw_v", "7"),
-			location{se, vertical}:   gpio.NewServoDriver(w, "se_v", "8"),
+type servo struct {
+	driver *gpio.ServoDriver
+}
+
+func New(w gpio.ServoWriter) *engine {
+	return &engine{
+		servos: map[location]*servo{
+			location{nw, horizontal}: &servo{driver: gpio.NewServoDriver(w, "nw_h", "1")},
+			location{ne, horizontal}: &servo{driver: gpio.NewServoDriver(w, "ne_h", "2")},
+			location{sw, horizontal}: &servo{driver: gpio.NewServoDriver(w, "sw_h", "3")},
+			location{se, horizontal}: &servo{driver: gpio.NewServoDriver(w, "se_h", "4")},
+			location{nw, vertical}:   &servo{driver: gpio.NewServoDriver(w, "nw_v", "5")},
+			location{ne, vertical}:   &servo{driver: gpio.NewServoDriver(w, "ne_v", "6")},
+			location{sw, vertical}:   &servo{driver: gpio.NewServoDriver(w, "sw_v", "7")},
+			location{se, vertical}:   &servo{driver: gpio.NewServoDriver(w, "se_v", "8")},
 		},
 	}
 }
